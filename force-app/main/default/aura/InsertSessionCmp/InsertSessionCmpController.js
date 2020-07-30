46b935e4b4b575fca217ff6220a8460cacb3da4f
arpit-vijay <arpit.vijay@blufy.com>
2020-07-30 18:56:50 +0530

    enrollment changes

--- force-app/main/default/aura/InsertSessionCmp/InsertSessionCmpController.js
+++ force-app/main/default/aura/InsertSessionCmp/InsertSessionCmpController.js
@@ -1,6 +1,34 @@
 ({
-    doInit : function(component, event, helper){
+    doInit: function (component, event, helper) {
         //component.set("v.showSpinner",true);
-        component.set("v.showComponent",true);        
+        component.set("v.showComponent", true);
+        helper.fillData(component, event);
     },
+    addRows: function (component, event, helper) {
+        console.log('calling');
+        let rowArr = component.get("v.rows");
+        console.log(rowArr);
+        let size = rowArr.length;
+        console.log(size);
+        let dayNumber = rowArr[size-1].DayNumber;
+       
+        let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        if(dayNumber == 6){
+            dayNumber = 0;
+        }else{
+            dayNumber++;
+        }
+        console.log(dayNumber);
+        console.log(days);
+        rowArr.push({
+            'index': size,
+            'Day': days[dayNumber],
+            'DayNumber':dayNumber,
+            'StartDt': '',
+            'Duration': 0,
+            'TeacherId': '',
+            'RoomId': '',
+        });
+        component.set("v.rows",rowArr);
+    }
 })
--- force-app/main/default/aura/InsertSessionCmp/InsertSessionCmpHelper.js
+++ force-app/main/default/aura/InsertSessionCmp/InsertSessionCmpHelper.js
@@ -1,5 +1,26 @@
 ({
-    helperMethod : function() {
-
+    fillData : function(component, event) {
+        let rowsArr = component.get("v.rows");
+        let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        let todayDate = new Date(); 
+        let noOfDay = todayDate.getDay();
+        for(let index = 0; index <=4; index++){           
+            rowsArr.push({
+                'index':index,
+                'Day':days[noOfDay],
+                'DayNumber':noOfDay,
+                'StartDt':'',
+                'Duration' : 0,
+                'TeacherId':'',
+                'RoomId':'',
+            });
+            noOfDay++;
+            if(noOfDay == 7){
+                noOfDay = 0;
+            }
+           
+        }
+        console.log('rowsArr',rowsArr);
+        component.set("v.rows",rowsArr);
     }
 })
